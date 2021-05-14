9ae013810b7afad37aef11fb7540bc2eb7e585eb
hieptm <hieptm@itsol.vn>
2021-05-14 14:45:46 +0700

    commit 20210514 - 2

--- src/app/config/auth.interceptor.ts
+++ src/app/config/auth.interceptor.ts
@@ -7,16 +7,20 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private jwtHelperService: JwtHelperService) {}
+  constructor(private jwtHelperService: JwtHelperService,
+              private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
     if (token && this.jwtHelperService.isTokenExpired(token, new Date().getTime())){
       request = request.clone({headers: request.headers.set('Authorization', `Bearer ${token}`)});
+    }else {
+      this.router.navigate(['login']).then(null);
     }
     return next.handle(request);
   }
